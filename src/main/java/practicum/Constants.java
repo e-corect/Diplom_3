a2a7249da1229af8b34a32a3bec933da79f4020e
egor.malyshev@theruntime.ru <egor.malyshev@theruntime.ru>
2025-02-18 15:54:23 +0300

    some refactoring

--- src/main/java/practicum/Constants.java
+++ src/main/java/practicum/Constants.java
@@ -20,13 +20,10 @@ public class Constants {
     /// Headers
     public static final String CONTENT_TYPE_HEADER_NAME = "Content-Type";
     public static final String JSON_HEADER_VALUE = "application/json";
-
     /// Элементы общей верхней панели
     public static final String CONSTRUCTOR_BTN_XPATH = ".//p[text()='Конструктор']";
-    public static final String ORDERS_FEED_BTN_XPATH = ".//*[text()='Лента Заказов']";
     public static final String LOGO_XPATH = ".//div[@class = 'AppHeader_header__logo__2D0X2']/a";
     public static final String PERSONAL_AREA_BTN_XPATH = ".//p[text()='Личный Кабинет']";
-
     /// Элементы главной страницы
     public static final String ACTIVE_TAB_XPATH = ".//div[contains(@class, 'current')]";
     public static final String ENTER_ACCOUNT_BTN_XPATH = ".//div/button[text()='Войти в аккаунт']";
@@ -36,7 +33,6 @@ public class Constants {
     public static final String SAUCES_BTN_XPATH = ".//div/span[text()='Соусы']";
     public static final String FILLINGS_BTN_XPATH = ".//div/span[text()='Начинки']";
     public static final String INGREDIENTS_MENU_CLASSNAME = "BurgerIngredients_ingredients__menuContainer__Xu3Mo";
-
     /// Элементы страницы login
     public static final String LOGIN_HEADER_TEXT = ".//h2[text()='Вход']";
     public static final String INPUT_EMAIL_XPATH = ".//div/input[@name='name']";
@@ -45,7 +41,6 @@ public class Constants {
     public static final String RECOVER_PWD_LINK_XPATH = ".//a[text()='Восстановить пароль']";
     public static final String REG_BTN_XPATH = ".//button[text()='Зарегистрироваться']";
     /// Элементы страницы регистрации
-    public static final String REG_HEADER_TEXT = ".//h2[text()='Регистрация']";
     public static final String USERNAME_INPUT_XPATH = ".//label[text() = 'Имя']//following-sibling::input";
     public static final String EMAIL_INPUT_XPATH = ".//label[text() = 'Email']//following-sibling::input";
     public static final String PWD_INPUT_XPATH = ".//div/input[@name='Пароль']";
@@ -55,11 +50,11 @@ public class Constants {
     /// Элементы страницы личного кабинета
     public static final String LOGOUT_BTN_XPATH = ".//button[text()='Выход']";
     public static final String PROFILE_TAB_XPATH = ".//*[text() = 'Профиль']";
-
+    /// Тестовые данные
     public static final String USER_PWD = "Qwertys1234567";
     public static final String INCORRECT_USER_PWD = "123";
-    public static final String[] FIRST_LVL_DOMAINS = {"example", "track", "template", "etc", "exe", "jpg", "ico", "rar"};
 
+    public static final String[] FIRST_LVL_DOMAINS = {"example", "track", "template", "etc", "exe", "jpg", "ico", "rar"};
     public static String getDomain(){
         Random random = new Random();
         return FIRST_LVL_DOMAINS[random.nextInt(FIRST_LVL_DOMAINS.length)];
